42ec8603c5482cb9ae05b3471ccdfa5a4c181167
yuma8686 <yuma4438@outlook.jp>
2025-05-29 13:38:42 +0900

    passfix

--- nav2_launch.py
+++ nav2_launch.py
@@ -7,44 +7,40 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # 定数としてパスを設定
     pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
-    
-    map_dir = LaunchConfiguration('map', default='/root/rmf_s502_real/map.yaml')
-    param_dir = LaunchConfiguration('params_file', default='/root/rmf_s502_real/limo_param.yaml')
-    
-    # ランチ引数を定義
+
+    map_dir = LaunchConfiguration('map')
+    param_dir = LaunchConfiguration('params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
     declare_map_cmd = DeclareLaunchArgument(
         'map',
         default_value='/root/rmf_s502_real/map.yaml',
         description='マップファイルへのフルパス')
-    
+
     declare_params_cmd = DeclareLaunchArgument(
         'params_file',
         default_value='/root/rmf_s502_real/limo_param.yaml',
         description='パラメータファイルへのフルパス')
-    
-    # Nav2を起動する
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Sim timeを使うか')
+
     nav_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(pkg_nav2_bringup, 'launch', 'bringup_launch.py')),
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_nav2_bringup, 'launch', 'bringup_launch.py')),
         launch_arguments={
             'map': map_dir,
             'params_file': param_dir,
-            'use_sim_time': 'true',
+            'use_sim_time': use_sim_time
         }.items()
     )
-    #バッテリー
-    # bat_status_launch = Node(
-    #         package="limo_battery_pub",
-    #         executable="limo_battery_pub",
-    #         output="screen"
-    #     )
 
-        
     return LaunchDescription([
         declare_map_cmd,
         declare_params_cmd,
-        # bat_status_launch,
+        declare_use_sim_time_cmd,
         nav_launch
     ])
-
